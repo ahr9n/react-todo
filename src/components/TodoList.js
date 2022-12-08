5de9bf775aa83ee452ce8cbfa4309196a04ad298
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-12-08 18:58:40 +0200

    Implement edit todo form

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,42 @@ import * as React from "react";
 
 import { TodoItem } from "./TodoItem";
 
-export function TodoList({ todos, handleRemoveTodo }) {
+export function TodoList({ todos, setTodoList }) {
+  const [todoEdit, setTodoEdit] = React.useState(null);
+  const [todoText, setTodoText] = React.useState("");
+
+  function handleEditTodo(id, name) {
+    setTodoEdit(id);
+    setTodoText(name);
+  }
+
+  function handleSaveTodo(id) {
+    if (!todoText) return;
+    let newTodoList = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.name = todoText;
+      }
+      return todo;
+    });
+    setTodoList(newTodoList);
+    setTodoEdit(null);
+  }
+
   return (
     <div className="container flex mx-auto w-full items-center justify-center">
       <ul className="flex flex-col p-4">
         {todos.map((todo) => (
-          <TodoItem key={todo.id} item={todo} handleRemove={handleRemoveTodo} />
+          <TodoItem
+            key={todo.id}
+            item={todo}
+            todos={todos}
+            setTodoList={setTodoList}
+            todoText={todoText}
+            todoEdit={todoEdit}
+            setTodoText={setTodoText}
+            handleSave={handleSaveTodo}
+            handleEdit={handleEditTodo}
+          />
         ))}
       </ul>
     </div>
